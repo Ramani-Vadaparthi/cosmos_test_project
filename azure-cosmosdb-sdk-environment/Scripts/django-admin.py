f5643fe10e6bcc5c09710ee3683929f43211cf93
Ramani-Vadaparthi <ramanivadaparthi@gmail.com>
2020-10-22 20:08:43 +0530

    'changes'

--- azure-cosmosdb-sdk-environment/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!d:\meas\lpa\sample project\cosmos_project\azure-cosmosdb-sdk-environment\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
